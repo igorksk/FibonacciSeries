c89fa11fd6afb3e6dd6e4b9de9581444530d8dc9
Igor <ihor.kaseko@aploris.com>
2022-04-25 13:07:00 +0300

    Add project files.

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Fibonacci(int n)
+            {
+                if (n == 0 || n == 1) return n;
+
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+
+            for (int n = 0; n < 10; n++)
+            {
+                int fib = Fibonacci(n);
+
+                Console.WriteLine($"{n} Fibonacci number = {fib}");
+            }
+        }
+    }
+}
